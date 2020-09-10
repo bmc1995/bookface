e025413ffbb5cfae0e200826aeb6548e0bacd283
bmc1995 <billymccrackin123@gmail.com>
2020-09-09 21:11:26 -0600

    Updates

---
+++ precache-manifest.2eee2de4c6428ec61e6dd9c7f1c74c1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0f02ead1853098ada7f723674e81c3",
+    "url": "/bookface/index.html"
+  },
+  {
+    "revision": "a175f152afdd501aa718",
+    "url": "/bookface/static/css/main.66350d41.chunk.css"
+  },
+  {
+    "revision": "35ed0e3d92dce3803358",
+    "url": "/bookface/static/js/2.3d1a964b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/bookface/static/js/2.3d1a964b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a175f152afdd501aa718",
+    "url": "/bookface/static/js/main.261168e1.chunk.js"
+  },
+  {
+    "revision": "5bfa4a737f2e41f802f9",
+    "url": "/bookface/static/js/runtime-main.31605173.js"
+  }
+]);
